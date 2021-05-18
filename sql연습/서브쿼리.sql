812bd34c4b32396c34535667bfeed4288e3db4c4
douzone-busan-bitacademy <douzone.busan.bitacademy@gmail.com>
2021-05-18 14:55:06 +0900

    서브쿼리 #3

--- sql연습/서브쿼리.sql
+++ sql연습/서브쿼리.sql
@@ -34,7 +34,7 @@
 						and concat(a.first_name, ' ', a.last_name) = 'Fai Bale');
 
  -- 2-1) where 절의 서브쿼리: 단일행
- -- 딘일행 연산자: =, >, <, >=, <=, <>, !=
+ -- 단일행 연산자: =, >, <, >=, <=, <>, !=
  
  -- 실습문제 1:
  -- 현재 전체사원의 평균 연봉보다 적은 급여를 받는 사원의  이름, 급여를 나타내세요.
@@ -96,3 +96,36 @@ group by a.title
 group by a.title
 order by avg_salary asc
    limit 0, 1;
+
+-- 2-2) where 절의 서브쿼리: 복수행
+-- 복수행 연산자: in, not in, any, all
+
+-- any 사용법
+-- 1. =any : in과 동일
+-- 2. >any, >=any : 최소값
+-- 3. <any, <=any : 최대값
+-- 4. <>any : not in 과 동일
+
+-- all 사용법
+-- 1. =all (x)
+-- 2. >all, >=all : 최대값
+-- 3. <all, <=all : 최소값
+ 
+-- 예제: 현재 급여가 50000 이상인 직원 이름 출력
+-- sol1
+select a.first_name, b.salary
+  from employees a, salaries b
+ where a.emp_no = b.emp_no
+   and b.to_date = '9999-01-01'
+   and (emp_no, salary) in (select emp_no, salary
+                              from salaries
+                             where to_date = '9999-01-01'
+                               and salary > 50000);
+
+
+;
+   
+
+
+
+ 
