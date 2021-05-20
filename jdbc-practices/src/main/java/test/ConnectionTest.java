625ae8d49e503f1057c25d92f680ee06cb678d56
douzone-busan-bitacademy <douzone.busan.bitacademy@gmail.com>
2021-05-20 10:05:12 +0900

    jdbc connection test #2

--- jdbc-practices/src/main/java/test/ConnectionTest.java
+++ jdbc-practices/src/main/java/test/ConnectionTest.java
@@ -7,13 +7,15 @@ import java.sql.SQLException;
 public class ConnectionTest {
 
 	public static void main(String[] args) {
+		Connection conn = null;
+		
 		try {
 			//1. JDBC Driver 로딩
 			Class.forName("org.mariadb.jdbc.Driver");
 			
-			//2.
-			String url = "jdbc:mysql://192.168.80.131:3307/webdb";
-			Connection conn = DriverManager.getConnection(url, "webdb", "webdb");
+			//2. 연결하기
+			String url = "jdbc:mysql://192.168.80.131:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
 			
 			//3. 연결 성공
 			System.out.println("ok:" + conn);
@@ -21,6 +23,14 @@ public class ConnectionTest {
 			System.out.println("드라이버 로딩 실패:" + e);
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
